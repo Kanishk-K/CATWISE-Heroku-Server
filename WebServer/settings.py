a6b16f7139f404a5debee34bea4153b4d8ebae1e
Kanishk-K <Kanishkkacholia@Gmail.com>
2021-03-30 10:34:54 -0500

    Removed Secret Key Holder

--- WebServer/settings.py
+++ WebServer/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '2au&s9jy$c!87=&senxq+i+n_pv4ya-#h$rv%j4zkx962!h127'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
